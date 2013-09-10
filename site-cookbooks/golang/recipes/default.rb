6b1d0c56106f8cf8194a05e114071d622cfa6287
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-09-10 20:46:38 +0900

    Add golang cookbook!

--- site-cookbooks/golang/recipes/default.rb
+++ site-cookbooks/golang/recipes/default.rb
@@ -6,3 +6,35 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+current_working_direcotry = '/home/vagrant/src'
+
+directory current_working_direcotry do
+  user 'vagrant'
+  group 'vagrant'
+  action :create
+end
+
+filename = "go1.1.2.linux-amd64.tar.gz"
+
+bash 'download golang binary' do
+  user 'vagrant'
+  group 'vagrant'
+  cwd current_working_direcotry
+
+  code <<-EOC
+    wget http://go.googlecode.com/files/#{filename}
+  EOC
+  not_if { File.exists?(current_working_direcotry + '/' + filename) }
+end
+
+bash 'install golang' do
+  cwd current_working_direcotry
+
+  code <<-EOC
+    sudo rm -rf /usr/local/go
+    tar -C /usr/local -xzf #{filename}
+    sudo sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
+  EOC
+  not_if 'which go'
+end
